ff342dae77635da0e5ff0c9a0f34246faa000100
Maximilian Liebsch <43824628+MaLIE2018@users.noreply.github.com>
2021-03-29 20:17:06 +0200

    better searchbox transision

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
 window.onload = (event) => {
-    let lens = document.getElementById("standardLens")
-    let insideSearchLens = document.getElementById("lensInSearchBox")
-    let dropdownContainer = document.getElementsByClassName("nav-item dropdown")[0]
-    let dropdownToggle = document.getElementsByClassName("dropdown-toggle")
-    console.log('dropdownToggle:', dropdownToggle)
+    let SearchContainer = document.getElementById("searchContainer")
 
 
-    // Collapse of the searchfield
-    lens.addEventListener("click", () => {
+    // Searchfield
+    SearchContainer.addEventListener("click", () => {
         setTimeout(function() {
             document.getElementById("searchField").focus()
         }, 20)
-        lens.classList.toggle("d-none")
-    })
+        SearchContainer.classList.toggle("border")
+        SearchContainer.classList.toggle("border-white")
+        SearchContainer.classList.toggle("input-hidden")
+        SearchContainer.classList.toggle("input-expanded")
 
-    insideSearchLens.addEventListener("click", () => {
-        lens.classList.remove("d-none")
     })
 
     // Dropdown menu navbar
+    let dropdownContainer = document.getElementById("SearchDropDown")
+    let dropdownToggle = document.getElementsByClassName("dropdown-toggle")
     dropdownContainer.addEventListener("mouseover", (event) => {
         let dropdown = document.getElementById("dropDown")
         dropdown.classList.add("show")
